9b7c14eff644a97aaa9b573ff6a43d98f679334d
netgrim <ebiron81@gmail.com>
2016-02-06 13:35:29 -0500

    add WindowTarget.cs and WindowQuery.cs

---
+++ MapKit.Core/ThemeNode/WindowQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace MapKit.Core
+{
+    class WindowQuery : ContainerNode
+    {
+        public const string ElementName = "window-query";
+
+        public override NodeType GetNodeType()
+        {
+            return new NodeType(ElementName, typeof(WindowQuery));
+        }
+    }
+}
---
+++ MapKit.Core/ThemeNode/WindowTarget.cs
@@ -0,0 +1,13 @@
+﻿namespace MapKit.Core
+{
+    class WindowTarget : ContainerNode
+    {
+        internal const string ElementName = "window-target";
+
+        public override NodeType GetNodeType()
+        {
+            return new NodeType(ElementName, typeof(WindowTarget));
+        }
+
+    }
+}
