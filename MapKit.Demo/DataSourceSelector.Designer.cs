a942aab3db0017214fc77ad8833d31bd8018e88a
netgrim <ebiron81@gmail.com>
2016-02-07 20:05:17 -0500

    add DataSourceSelector

---
+++ MapKit.Demo/DataSourceSelector.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MapKit.Demo
+{
+    partial class DataSourceSelector
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.OkButton = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName});
+            this.listView.FullRowSelect = true;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(421, 211);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 116;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OkButton.Location = new System.Drawing.Point(277, 229);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(75, 23);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "&OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.Location = new System.Drawing.Point(358, 229);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancel.TabIndex = 2;
+            this.cmdCancel.Text = "&Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            // 
+            // DataSourceSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 264);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.listView);
+            this.Name = "DataSourceSelector";
+            this.Text = "DataSetSelector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button cmdCancel;
+    }
+}
---
+++ MapKit.Demo/DataSourceSelector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MapKit.Core;
+
+namespace MapKit.Demo
+{
+    public partial class DataSourceSelector : Form
+    {
+        private List<DataSourceType> _dataSources;
+        public DataSourceSelector()
+        {
+            InitializeComponent();
+        }
+
+        public List<DataSourceType> DataSources {
+            get
+            {
+                return _dataSources;
+            }
+            set
+            {
+                _dataSources = value;
+                listView.Items.Clear();
+                foreach (var source in _dataSources)
+                    AddItem(source);
+                
+                if (listView.Items.Count > 0)
+                    listView.SelectedIndices.Add(0);
+            }
+        }
+
+        private void AddItem(DataSourceType source)
+        {
+            var item = listView.Items.Add(source.Name);
+            item.Tag = source;
+        }
+
+        public DataSourceType SelectedDataSource
+        {
+            get
+            {
+                return listView.SelectedItems.Count > 0
+                    ? listView.SelectedItems[0].Tag as DataSourceType
+                    : null;
+            }
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            if (SelectedDataSource != null)
+                OkButton.PerformClick();
+        }
+    }
+}
